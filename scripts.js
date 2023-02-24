24e132a9bdb19c7e72cf2feaa0eff7197e05d2f4
KBeKind <79383539+KBeKind@users.noreply.github.com>
2023-02-23 20:30:27 -0600

    done

--- scripts.js
+++ scripts.js
@@ -1,99 +1,107 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 
-    
+
     const rocket = document.getElementById("rocket");
-    
+
     const takeoffButton = document.getElementById("takeoff");
 
     const landingButton = document.getElementById("landing");
-    
+
     const abortMissionButton = document.getElementById("missionAbort");
 
     const upButton = document.getElementById("up");
 
     const downButton = document.getElementById("down");
-    
+
     const leftButton = document.getElementById("left");
-    
+
     const rightButton = document.getElementById("right");
-    
+
     const flightStatus = document.getElementById("flightStatus");
-    
+
     const shuttleBackground = document.getElementById("shuttleBackground");
 
     const spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
 
 
-    takeoffButton.addEventListener("click", function() {
+    takeoffButton.addEventListener("click", function () {
         if (confirm("Confirm that the shuttle is ready for takeoff.")) {
             flightStatus.textContent = "Shuttle in flight.";
             shuttleBackground.style.backgroundColor = "blue";
             spaceShuttleHeight.textContent = "10000";
+            rocket.style.top = "240px";
 
-            landingButton.addEventListener("click", function() {
+            landingButton.addEventListener("click", function () {
                 alert("The shuttle is landing. Landing gear engaged.");
                 flightStatus.textContent = "The shuttle has landed.";
                 shuttleBackground.style.backgroundColor = "green";
                 spaceShuttleHeight.textContent = "0";
+                rocket.style.top = "250px";
+                rocket.style.left = "280px";
             });
 
-            abortMissionButton.addEventListener("click", function() {
+            abortMissionButton.addEventListener("click", function () {
                 confirm("Confirm that you want to abort the mission.");
                 flightStatus.textContent = "Mission aborted.";
                 shuttleBackground.style.backgroundColor = "green";
-                spaceShuttleHeight.textContent = "0"; 
+                spaceShuttleHeight.textContent = "0";
+                rocket.style.top = "250px";
+                rocket.style.left = "280px";
             });
 
-            upButton.addEventListener("click", function() {
-                spaceShuttleHeight.textContent = parseInt(spaceShuttleHeight.textContent) + 10000;
+            upButton.addEventListener("click", function () {
 
                 let currentTop = rocket.getAttribute("style");
-                
+
                 let nextTop = currentTop.split('p');
 
                 currentTop = nextTop[1].slice(2);
-
-                rocket.style.top = (parseInt(currentTop) - 10) + "px";
+                if (currentTop > 30) {
+                    rocket.style.top = (parseInt(currentTop) - 10) + "px";
+                    spaceShuttleHeight.textContent = parseInt(spaceShuttleHeight.textContent) + 10000;
+                }
 
             });
 
-            downButton.addEventListener("click", function() {
-                spaceShuttleHeight.textContent = parseInt(spaceShuttleHeight.textContent) - 10000;
+            downButton.addEventListener("click", function () {
 
                 let currentTop = rocket.getAttribute("style");
-                
+
                 let nextTop = currentTop.split('p');
 
                 currentTop = nextTop[1].slice(2);
-
-                rocket.style.top = (parseInt(currentTop) + 10) + "px";
-                
+                if (currentTop < 250) {
+                    spaceShuttleHeight.textContent = parseInt(spaceShuttleHeight.textContent) - 10000;
+                    rocket.style.top = (parseInt(currentTop) + 10) + "px";
+                }
             });
 
-            leftButton.addEventListener("click", function() {
+            leftButton.addEventListener("click", function () {
                 let currentLeft = rocket.getAttribute("style");
-                
+
                 let nextLeft = currentLeft.split('p');
 
                 currentLeft = nextLeft[2].split(":")
                 let test = currentLeft[1].slice(1);
+                if (parseInt(test) > 15) {
 
-                rocket.style.left = (parseInt(test) - 10) + "px";
+                    rocket.style.left = (parseInt(test) - 10) + "px";
+                }
             });
 
-            rightButton.addEventListener("click", function() {
-           
+            rightButton.addEventListener("click", function () {
+
                 let currentLeft = rocket.getAttribute("style");
-                
+
                 let nextLeft = currentLeft.split('p');
 
                 currentLeft = nextLeft[2].split(":")
                 let test = currentLeft[1].slice(1);
-                if (parseInt(test) < 100) {
+                if (parseInt(test) < 560) {
 
-                rocket.style.left = (parseInt(test) + 10) + "px"; 
+                    rocket.style.left = (parseInt(test) + 10) + "px";
                 }
             });
         };
